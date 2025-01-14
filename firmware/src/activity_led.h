a041f0d208c647d9eb05559c434079a4cbf5b11a
Andr0x <andrea.pede@gmail.com>
2025-01-14 09:28:01 +0100

    Update activity_led.h

--- firmware/src/activity_led.h
+++ firmware/src/activity_led.h
@@ -1,6 +1,7 @@
 #ifndef _ACTIVITY_LED_H_
 #define _ACTIVITY_LED_H_
 
+void setup_activity_led(); // Add this line for NeoPixel initialization
 void activity_led_on();
 void activity_led_off_maybe();
 
